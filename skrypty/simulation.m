66b62df5fe98d2962f9946a64c9de6626c78ca79
Kuszki <l.drozdz@o2.pl>
2023-02-28 18:02:34 +0100

    Sppedup

--- skrypty/simulation.m
+++ skrypty/simulation.m
@@ -8,19 +8,23 @@ pkg load signal;
 
 addpath("~/Projekty/Octave-FWT-Utils");
 
+#profile clear;
+#profile on;
+
 # iterations params
 iters_a = 5e4;#2e4;
-iters_b = 5e0;#5e1;
+iters_b = 5e1;#5e1;
 
 # wavelet params
 wname = 'db2';
 ndec = 2;
 nsam = 8;
-num = 1;
+num = 0;
 
 # output settings
 calc_in_uc = 1;
-show_prog = 1;
+calc_out_uc = 1;
+show_prog = 0;
 
 # sampling params
 fs = 48e3;
@@ -42,27 +46,29 @@ amp_b = 3.3;
 t_exp = 20;
 t_diff = 3;
 
-# objects randoms
-var_s_t = (3*t_diff^2)/(18);
-var_r_s = 2e-5/3;
-var_r_a = 1e-5/3;
-var_r_d = 1.65e-6;#1.65e-6;
-
 # adc settings
 nq = 256;
 vp =  3.0;
 vn = -3.0;
 aq = (vp - vn) / nq;
 
+# objects randoms
+var_s_t = (3*t_diff^2)/(18);
+var_r_s = 2e-5/3;
+var_r_a = 1e-5/3;
+var_r_c = (aq^2)/12;
+var_r_d = 1.65e-6;
+
 # timing evals
 ns = nsam * iters_b;
 Ts = 1/fs;
 T0 = 1/f0;
+pi2 = 2*pi;
 
 # input signal
-f_in_1 = @(x, fi) am_1*sin(2*pi*f_1*x + ph_1 + fi);
-f_in_2 = @(x, fi) am_2*sin(2*pi*f_2*x + ph_2 + fi);
-f_in_3 = @(x, fi) am_3*sin(2*pi*f_3*x + ph_3 + fi);
+f_in_1 = @(x, fi) am_1*sin(pi2*f_1*x + ph_1 + fi);
+f_in_2 = @(x, fi) am_2*sin(pi2*f_2*x + ph_2 + fi);
+f_in_3 = @(x, fi) am_3*sin(pi2*f_3*x + ph_3 + fi);
 f_in = @(x) f_in_1(x, 0) + f_in_2(x, 0) + f_in_3(x, 0);
 
 # error signals
@@ -85,7 +91,6 @@ f_fil_b_cmp = @(x) 1 / (1 + i * (x/fb));
 f_fil_b_amp = @(x) abs(f_fil_b_cmp(x));
 f_fil_b_phi = @(x) atan(imag(f_fil_b_cmp(x))./real(f_fil_b_cmp(x)));
 
-A = lin_ident(@(x) fwt(x, wname, ndec), nsam);
 sq2_4 = 4*sqrt(2); sq3 = sqrt(3);
 A = [ ...
 (5-sq3)/16 (5+sq3)/16 (3+3*sq3)/16 (5+3*sq3)/16 (3+sq3)/16 (3-sq3)/16 (5-3*sq3)/16 (3-3*sq3)/16; ...
@@ -103,8 +108,8 @@ elen_v = iters_a * ns;
 temp = gen_randt(iters_a, var_s_t, 'w') + t_exp;
 phi = rand(1, iters_a) * T0;
 
-out_E = zeros(elen_v, nsam + 1);
-f_st = zeros(nsam - 1, nsam);
+errs = zeros(elen_v, 1);
+out_E = zeros(elen_m, nsam);
 
 step = floor(iters_a*iters_b / 10);
 curr = 1;
@@ -125,53 +130,68 @@ tic; for i = 1 : iters_a
 
   # inject signal noise
   ys = y;
-#  ys = y + gen_randn(ns, var_r_s, 'w');
+  ys = y + gen_randn(ns, var_r_s, 'w');
 
   # perform converter part tasks
   ya = ys;
   ya = ya + f_tm_a(temp(i));
-#  ya = ya + gen_randu(ns, var_r_a, 'w');
+
+  ya = ya + gen_randu(ns, var_r_a, 'w');
   ya = ya ...
-#       + f_err_1(x, f_fil_a_amp(f_1), f_fil_a_phi(f_1)) ...
-#       + f_err_2(x, f_fil_a_amp(f_2), f_fil_a_phi(f_2)) ...
-#       + f_err_3(x, f_fil_a_amp(f_3), f_fil_a_phi(f_3)) ...
+       + f_err_1(x, f_fil_a_amp(f_1), f_fil_a_phi(f_1)) ...
+       + f_err_2(x, f_fil_a_amp(f_2), f_fil_a_phi(f_2)) ...
+       + f_err_3(x, f_fil_a_amp(f_3), f_fil_a_phi(f_3)) ...
   ;
 
   # perform amplifier part tasks
   yb = ya;
   yb = yb ...
-#       + f_err_1(x, f_fil_b_amp(f_1), f_fil_b_phi(f_1)) ...
-#       + f_err_2(x, f_fil_b_amp(f_2), f_fil_b_phi(f_2)) ...
-#       + f_err_3(x, f_fil_b_amp(f_3), f_fil_b_phi(f_3)) ...
+       + f_err_1(x, f_fil_b_amp(f_1), f_fil_b_phi(f_1)) ...
+       + f_err_2(x, f_fil_b_amp(f_2), f_fil_b_phi(f_2)) ...
+       + f_err_3(x, f_fil_b_amp(f_3), f_fil_b_phi(f_3)) ...
   ;
   yb = amp_b * yb;
   yb = yb + f_tm_b(temp(i));
 
   # perform adc tasks
   yc = yb;
-#  yc = f_adc(yc);
+  yc = yc + gen_randu(ns, var_r_c, 'w');
 
   # calc input error
-  in_err = yc - yi;
-  out_E(ns*(i-1)+1:ns*i,1) = in_err;
+  cerr = transpose(yc - yi);
 
-  for j = 1 : nsam
+  for j = 1 : nsam : ns
 
-    [out_err, f_st(:,j)] = filter(A(j,:), 1, in_err, f_st(:,j));
-#    out_err = out_err + gen_randn(ns, var_r_d, 'w');
+    out_E(curr,:) = A*cerr(j:j+nsam-1);
+    curr = curr + 1;
 
-    out_E(ns*(i-1)+1:ns*i,j+1) = out_err;
+  end
 
+  if calc_in_uc
+    errs(ns*(i-1)+1:ns*i) = cerr;
   end
 
 end; toc;
 
-if calc_in_uc
+for j = 1 : nsam
+#  out_E(:,j) = out_E(:,j) + transpose(gen_randu(elen_m, var_r_d, 'w'));
+end
+
+if calc_in_uc || calc_out_uc
   printf("n\tu\tc\ts\tw\n");
-  for i = 1 : nsam + 1
-    if i == 1 || num == i-1 || num == 0
+end
+
+if calc_in_uc
+  [u_in, c_in, s_in, w_in] = get_uncertainty(errs);
+  printf("%d\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", 0, ...
+         u_in * 1000, c_in, s_in * 1000, w_in * 1000000);
+end
+
+if calc_out_uc
+  for i = 1 : nsam
+    if num == i || num == 0
       [u_in, c_in, s_in, w_in] = get_uncertainty(out_E(:,i));
-      printf("%d\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", i-1, ...
+      printf("%d\t%0.2f\t%0.2f\t%0.2f\t%0.2f\n", i, ...
              u_in * 1000, c_in, s_in * 1000, w_in * 1000000);
     end
   end
@@ -180,6 +200,9 @@ end
 if num > 0
   hist(out_E(:,num+1), 100);
 else
-  hist(out_E(:,1), 100);
+  hist(errs, 100);
 end
+
+#profile off;
+#profshow();
 
