e31b7bc395b7df65fc547d87b6d2172d2c744ed3
Kuszki <l.drozdz@o2.pl>
2023-03-09 17:21:21 +0100

    Update test

--- skrypty/gen_cohplot.m
+++ skrypty/gen_cohplot.m
@@ -8,26 +8,25 @@ addpath("~/Projekty/Octave-FWT-Utils")
 
 list = "nust";
 
-m = linspace(-1, 1, 50) .^ 3;
+m = linspace(0.05, 0.99, 51) .^ 2;
+m = [ -flip(m) 0 m];
 
 for a = list
-  for b = list
 
-    hv = rv = zeros(1, length(m));
+  hv = rv = zeros(1, length(m));
 
-    for i = 1 : length(m)
+  for i = 1 : length(m)
 
-      fun = @(x) gen_coherence(1, 1, a, b, 's', 1e5, m(i));
-      [h, r] = pararrayfun(nproc-1, fun, 1:500);
+    fun = @(x) gen_coherence(1, 1, a, a, 'u', 1e5, m(i));
+    [h, r] = pararrayfun(nproc-1, fun, 1:10);
 
-      hv(i) = mean(h);
-      rv(i) = mean(r);
-
-    end
-
-    v = transpose([ [rv]; [hv] ]);
-    save('-ascii', sprintf("../archiwa/cohers/%s_%s.txt", a, b), "v")
+    hv(i) = mean(h);
+    rv(i) = mean(r);
 
   end
+
+  v = transpose([ [-1 rv 1]; [-1 hv 1] ]);
+  save('-ascii', sprintf("../archiwa/cohers/cor_%s.txt", a), "v")
+
 end
 
