86807b40fb2e275dc59bfca3ac84ec6754534826
Kuszki <l.drozdz@o2.pl>
2023-08-01 22:18:06 +0200

    Fix pdf version bug

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-PDFV_LO='{"SelectPdfVersion":{"type":"long","value":"1"}}'
-PDFV_IS='1.4'
-
 REMOVE=false
 CONVERT=false
 BUILD=true
@@ -32,7 +29,7 @@ while [ "$1" != "" ]; do
 done
 
 [ $REMOVE == true ] && rm budowa/*
-[ $CONVERT == true ] && libreoffice --convert-to "pdf:draw_pdf_Export:$PDFV_LO" obrazki/*.odg --outdir obrazki 
-[ $CONVERT == true ] && inkscape -D obrazki/*.svg --export-type pdf --export-pdf-version "$PDFV_IS"
+[ $CONVERT == true ] && libreoffice --convert-to pdf obrazki/*.odg --outdir obrazki 
+[ $CONVERT == true ] && inkscape -D obrazki/*.svg --export-type pdf
 [ $BUILD == true ] && latexmk --shell-escape -output-directory=budowa -pdflua thesis.tex
 
