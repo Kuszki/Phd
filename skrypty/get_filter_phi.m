ffb319d787480661735160cd79e7ecbea46a04a3
Kuszki <l.drozdz@o2.pl>
2024-02-27 22:01:00 +0100

    Fix freq issue

--- skrypty/get_filter_phi.m
+++ skrypty/get_filter_phi.m
@@ -1,8 +1,8 @@
 function [phi] = get_filter_phi(w)
 
-	wac = (2*pi)/(6.0e+3*7.0e-12);
+	wac = (1)/(6.0e+3*7.0e-12);
 
-	phi = -6.821e-13*w^2 - 5.462e-7*w;
+	phi = -6.76e-13*w^2 - 5.23e-7*w;
 	phi = phi + atan(-w/wac);
 
 end
